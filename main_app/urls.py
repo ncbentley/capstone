19bb4a104d36615fa9002e1c884d6b7b919f3815
Nico Bentley <ncbentley4@gmail.com>
2020-07-08 16:42:23 -0600

    views skeleton

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name="home"),
+    path('projects/', views.projects, name="projects"),
+    path('projects/<int:project_id>/', views.project, name="project"),
+    path('projects/<int:project_id>/sprints/', views.sprints, name="sprints"),
+    path('projects/<int:project_id>/sprints/<int:sprint_id>/', views.sprint, name="sprint"),
+    path('projects/<int:project_id>/pages/', views.pages, name="pages"),
+    path('projects/<int:project_id>/pages/<int:page_id>/', views.page, name="page"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    pass
+
+def projects(request):
+    pass
+
+def project(request, project_id):
+    pass
+
+def sprints(request, project_id):
+    pass
+
+def sprint(request, project_id, sprint_id):
+    pass
+
+def pages(request, project_id):
+    pass
+
+def page(request, project_id, page_id):
+    pass
