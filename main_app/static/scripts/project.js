dc1fd4f64acfa8772f2119a55c413febe4dc9587
Nico Bentley <ncbentley4@gmail.com>
2020-07-14 10:01:50 -0600

    disabled sprint no click

--- main_app/static/scripts/project.js
+++ main_app/static/scripts/project.js
@@ -1,13 +1,13 @@
 $(function() {
-    $('.sprint:not(.disabled), .page').click(function(event) {
+    $('.sprint, .page').click(function(event) {
         node = event.target;
         while (!$(node).is('.sprint') && !$(node).is('.page')) {
             node = node.parentNode;
         }
-        window.location.href = $(node).data('url');
+        if (!$(node).is('.disabled')) window.location.href = $(node).data('url');
     })
     $('.sprint.completed input[type="checkbox"]').prop("checked", true);
-    $($('.sprint:not(.completed)')[$('.sprint:not(.completed)').length - 1]).nextAll().addClass('disabled');
+    $($('.sprint:not(.completed)')[0]).nextAll().addClass('disabled');
     $('.project').click(function(event) {
         node = event.target;
         while (!$(node).is('.project')) {
