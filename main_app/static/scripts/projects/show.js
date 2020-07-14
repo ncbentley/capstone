52749a1c70024b7548e23153323a941d33a588c2
Nico Bentley <ncbentley4@gmail.com>
2020-07-14 10:20:40 -0600

    modal close buttons

--- main_app/static/scripts/projects/show.js
+++ main_app/static/scripts/projects/show.js
@@ -7,12 +7,5 @@ $(function() {
         if (!$(node).is('.disabled')) window.location.href = $(node).data('url');
     })
     $('.sprint.completed input[type="checkbox"]').prop("checked", true);
-    $($('.sprint:not(.completed)')[0]).nextAll().addClass('disabled');
-    $('.project').click(function(event) {
-        node = event.target;
-        while (!$(node).is('.project')) {
-            node = node.parentNode;
-        }
-        window.location.href = $(node).data('url')
-    })
+    if (userType === 'dev') $($('.sprint:not(.completed)')[0]).nextAll().addClass('disabled');
 })
